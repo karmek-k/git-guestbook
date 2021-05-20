06d2c575e28cac5ee7af45934956a3f49963c4ae
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-20 15:41:15 +0200

    Add Guestbook validators

--- src/Entity/Guestbook.php
+++ src/Entity/Guestbook.php
@@ -6,6 +6,7 @@ use App\Repository\GuestbookRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=GuestbookRepository::class)
@@ -23,6 +24,7 @@ class Guestbook
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="guestbooks")
      * @ORM\JoinColumn(nullable=false)
      */
+    #[Assert\NotNull]
     private $owner;
 
     /**
@@ -33,6 +35,8 @@ class Guestbook
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Assert\Length(min: 5, max: 20)]
+    #[Assert\NotBlank]
     private $name;
 
     public function __construct()
