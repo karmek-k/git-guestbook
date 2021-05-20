dd9df0830bebd88200450eb2f17ccc0aa96f15d2
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-20 15:32:06 +0200

    Render the guestbook form

--- src/Controller/GuestbooksController.php
+++ src/Controller/GuestbooksController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Guestbook;
+use App\Form\GuestbookType;
 use App\Repository\GuestbookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,6 +19,16 @@ class GuestbooksController extends AbstractController
 
         return $this->render('guestbooks/index.html.twig', [
             'guestbooks' => $guestbooks,
+        ]);
+    }
+
+    #[Route('/guestbooks/create', name: 'guestbooks_create', methods: ['GET', 'POST'])]
+    public function create(): Response
+    {
+        $form = $this->createForm(GuestbookType::class);
+
+        return $this->render('guestbooks/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
