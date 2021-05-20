c9801152b7f58a201686b585d7017f3b04311308
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-20 15:21:42 +0200

    List the current user's guestbooks

--- src/Controller/GuestbooksController.php
+++ src/Controller/GuestbooksController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Guestbook;
+use App\Repository\GuestbookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GuestbooksController extends AbstractController
 {
-    #[Route('/guestbooks', name: 'guestbooks')]
-    public function index(): Response
+    #[Route('/guestbooks', name: 'guestbooks_list')]
+    public function index(GuestbookRepository $repo): Response
     {
+        $user = $this->getUser();
+        $guestbooks = $repo->findBy(['owner' => $user]);
+
         return $this->render('guestbooks/index.html.twig', [
-            'controller_name' => 'GuestbooksController',
+            'guestbooks' => $guestbooks,
         ]);
     }
 }
