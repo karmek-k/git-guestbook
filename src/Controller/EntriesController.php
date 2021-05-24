a315b37d3d470315f1626648e325d3cd8a4066d7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-24 13:55:53 +0200

    Create entry_create route, change guestbook_id name

--- src/Controller/EntriesController.php
+++ src/Controller/EntriesController.php
@@ -9,11 +9,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EntriesController extends AbstractController
 {
-    #[Route('/entries/{guestbook_id}', name: 'entries_list')]
+    #[Route('/entries/{guestbook}', name: 'entries_list')]
     public function entriesList(Guestbook $guestbook): Response
     {
         return $this->render('entries/list.html.twig', [
             'guestbook' => $guestbook,
         ]);
+    }
+
+    #[Route('/entries/{guestbook}/create', name: 'entry_create')]
+    public function create(Guestbook $guestbook): Response
+    {
+        // you cannot add entries to your own guestbook
+        if ($this->getUser() == $guestbook->getOwner()) {
+            // TODO: create a flash message
+            // $this->addFlash('error', 'You cannot add entries to your own guestbook')
+        
+            return $this->redirectToRoute('entries_list', [
+                'guestbook' => $guestbook->getId()
+            ]);
+        }
+
+        return new Response('TODO: implement this');
     }
 }
