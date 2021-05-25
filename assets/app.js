3eac84ab24d4b17fca566cc4e6071ef7dabe6717
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-25 17:25:53 +0200

    Make the navbar responsive

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,10 @@
 // Materialize CSS
-import "materialize-css/dist/css/materialize.min.css";
-import "materialize-css/dist/js/materialize.min";
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
+
+// custom scripts
+import './navbarCollapse';
 
 // start the Stimulus application
-import "./bootstrap";
+import './bootstrap';
+
