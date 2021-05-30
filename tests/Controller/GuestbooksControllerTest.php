f63d9a7528e1ff1c46c4f74d09cb2f76281ef4b2
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-30 19:27:28 +0200

    Add testFetchingGuestbooks

--- tests/Controller/GuestbooksControllerTest.php
+++ tests/Controller/GuestbooksControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class GuestbooksControllerTest extends WebTestCase
@@ -13,5 +14,24 @@ class GuestbooksControllerTest extends WebTestCase
         $client->request('GET', '/guestbooks');
 
         $this->assertResponseRedirects();
+    }
+
+    public function testFetchingGuestbooks(): void
+    {
+        $client = static::createClient();
+
+        /** @var UserRepository $userRepo */
+        $userRepo = static::$container->get(UserRepository::class);
+        $user = $userRepo->findOneBy(['username' => 'user']);
+        $guestbookCount = $user->getGuestbooks()->count();
+
+        $client->loginUser($user);
+        $crawler = $client->request('GET', '/guestbooks');
+        // minus the header row
+        $tableRowCount = $crawler->filter('tr')->count() - 1;
+    
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'My Guestbooks');
+        $this->assertEquals($guestbookCount, $tableRowCount);
     }
 }
